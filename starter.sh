3d9ef84e1c33383923ef357808064fd1148cf25b
Marc Gueury <marc.gueury@oracle.com>
2025-11-10 15:50:02 +0100

    v3.0

--- starter.sh
+++ starter.sh
@@ -6,22 +6,22 @@ STARTER_DIR=".starter"
 if [ -f $STARTER_DIR/starter.sh ]; then
   echo "-- Synchronizing the source files with directory $STARTER_DIR --"
   if [ -d app ]; then
-    rsync -av app/ $STARTER_DIR/src/app
+    rsync -avh app/ $STARTER_DIR/src/app
   fi
   if [ -d src ]; then
-    rsync -av src/ $STARTER_DIR/src/app/src
+    rsync -avh src/ $STARTER_DIR/src/app/src
     if [ -f build_app.sh ]; then
       cp build_app.sh $STARTER_DIR/src/app/.
     fi
   fi
   if [ -d db ]; then
-    rsync -av db/ $STARTER_DIR/src/db
+    rsync -avh db/ $STARTER_DIR/src/db
   fi
   if [ -d ui ]; then
-    rsync -av ui/ $STARTER_DIR/src/ui
+    rsync -avh ui/ $STARTER_DIR/src/ui
   fi
   if [ -f terraform ]; then
-    rsync -R terraform/ $STARTER_DIR/target/app_terraform
+    rsync -avh terraform/ $STARTER_DIR/target/app_terraform
     cp -R $STARTER_DIR/target/app_terraform/* $STARTER_DIR/src/terraform
   fi
   if [ -f terraform.tfvars ]; then
