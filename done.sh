d13873f4fa5ef94362174ca61d30abc281713d23
Marc Gueury <marc.gueury@oracle.com>
2025-11-11 12:43:58 +0100

    v3.0

--- done.sh
+++ done.sh
@@ -10,9 +10,15 @@ echo
 echo "Build done"
 
 # Do not show the Done URLs if after_build.sh exists 
-if [ "$UI_URL" != "" ]; then
-  append_done "LiteLLM installed:"
-  append_done "- URL: $UI_URL:8080/ui"
+append_done "LiteLLM installed:"
+append_done "- URL: $UI_URL:8080/ui"
+append_done ""
+append_done "OpenAI compatible URL"
+append_done "- $UI_URL:8080/v1"
+append_done "- https://${APIGW_HOSTNAME}/${TF_VAR_prefix}/v1"
+append_done
+append_done "APIKEY: $TF_VAR_db_password"
+append_done "MODEL: oci_cohere_command_latest (see config.yaml)"
 
 elif [ ! -f $FILE_DONE ]; then
   echo "-" > $FILE_DONE  
