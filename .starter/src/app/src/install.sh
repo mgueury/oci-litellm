b6e20dc890f86c61177e17cfbe05627880a42f6a
Marc Gueury <marc.gueury@oracle.com>
2025-11-19 12:42:31 +0100

    v3.0

--- .starter/src/app/src/install.sh
+++ .starter/src/app/src/install.sh
@@ -2,8 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-. ./env.sh
-
 # https://yum.oracle.com/oracle-linux-python.html
 
 sudo dnf install -y python3.12 python3.12-pip python3-devel
@@ -15,38 +13,3 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 uv venv myenv
 source myenv/bin/activate
 uv pip install -r requirements.txt
-
-# Prima
-sudo dnf module enable -y nodejs:20
-sudo dnf install -y nodejs
-# LiteLLM bug
-python -m prisma generate --schema myenv/lib64/python3.12/site-packages/litellm/proxy/schema.prisma
-
-# Postgres (LiteLLM config)
-sudo dnf install -y @postgresql
-sudo /usr/bin/postgresql-setup --initdb
-sudo systemctl enable postgresql
-sudo sed -i "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
-sudo systemctl restart postgresql
-
-# . ./env.sh
-# echo $DATABASE_URL
-# sudo su - postgres
-# psql 
-# \l
-
-cd /tmp
-sudo -u postgres psql -c "CREATE USER litellm_user WITH ENCRYPTED PASSWORD '$TF_VAR_db_password';"
-sudo -u postgres psql -c "CREATE DATABASE litellm_db OWNER litellm_user;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE litellm_db TO litellm_user;"
-cd -
-
-# Config.yaml
-sed -i "s/##TF_VAR_compartment_ocid##/$TF_VAR_compartment_ocid/" config.yaml
-sed -i "s/##TF_VAR_region##/$TF_VAR_region/" config.yaml
-sed -i "s/##TF_VAR_current_user_ocid##/$TF_VAR_current_user_ocid/" config.yaml
-sed -i "s/##TF_VAR_tenancy_ocid##/$TF_VAR_tenancy_ocid/" config.yaml
-
-# Patch DAC/Cohere
-cp myenv/lib64/python3.12/site-packages/litellm/llms/oci/chat/transformation.py myenv/lib64/python3.12/site-packages/litellm/llms/oci/chat/transformation.py.backup
-cp oci_litellm/transformation.py myenv/lib64/python3.12/site-packages/litellm/llms/oci/chat/transformation.py
--- .starter/src/app/src/start.sh
+++ .starter/src/app/src/start.sh
@@ -4,5 +4,4 @@ cd $SCRIPT_DIR
 . ./env.sh
 
 source myenv/bin/activate
-# litellm --port 8080 --detailed_debug 2>&1 | tee app.log
-litellm --port 8080 --config config.yaml --detailed_debug 2>&1 | tee app.log
+python app.py 2>&1 | tee app.log
