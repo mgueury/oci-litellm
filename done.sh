6c80063290a836ba57efbb3cac4a35fea1bacf8c
Marc Gueury <marc.gueury@oracle.com>
2025-11-19 15:24:50 +0100

    v3.0

--- done.sh
+++ done.sh
@@ -23,7 +23,7 @@ append_done "  API KEY : $TF_VAR_db_password"
 append_done "  MODEL   : oci_cohere_command_latest (see config.yaml)"
 append_done ""
 append_done "curl https://${APIGW_HOSTNAME}/${TF_VAR_prefix}/v1/completions \\"
-append_done "-H "Content-Type: application/json" \\"
+append_done "-H \"Content-Type: application/json\" \\"
 append_done "-H \"Authorization: Bearer $TF_VAR_db_password\" \\"
 append_done "-d '{\"model\": \"oci_cohere_command_latest\", \"prompt\": \"Who are you\", \"max_tokens\": 200}'"
 
