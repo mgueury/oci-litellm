0ba870d57680e729ba04c62753e2212f425327ea
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-31 01:00:13 +0200

    v3.0

--- src/app/rest/start.sh
+++ src/app/rest/start.sh
@@ -5,5 +5,4 @@ cd $SCRIPT_DIR
 . ./env.sh
 
 source myenv/bin/activate
-export PROXY_BASE_URL=https://${APIGW_HOSTNAME}
 litellm --port 8080 --config config.yaml --detailed_debug 2>&1 | tee rest.log
--- src/terraform/apigw.tf
+++ src/terraform/apigw.tf
@@ -74,7 +74,7 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment" {
       methods = [ "ANY" ]
       backend {
         type = "HTTP_BACKEND"
-        url    = "http://${local.apigw_dest_private_ip}/$${request.path[pathname]}"
+        url    = "http://${local.apigw_dest_private_ip}:8080/$${request.path[pathname]}"
       }
       # Forward the JWT access_token using Bearer
       # To decode, and get the username  
@@ -88,7 +88,15 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment" {
               values = [
                 "Bearer $${request.auth[access_token]}",
               ]
-            }          
+            }         
+            items {
+              name = "X-Forwarded-Host"
+              values = ["$${request.headers[Host]}"]
+            }
+            items {
+              name = "X-Forwarded-Proto"
+              values = ["https"]
+            }            
           }
         }
       }         
