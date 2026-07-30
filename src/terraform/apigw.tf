de22c547c1c9b87bd391c8e0dded390b69716a59
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-30 20:23:46 +0200

    v3.0

--- src/terraform/apigw.tf
+++ src/terraform/apigw.tf
@@ -58,7 +58,7 @@ resource "oci_apigateway_deployment" "starter_apigw_deployment" {
   compartment_id = local.lz_app_cmp_ocid
   display_name   = "${var.prefix}-apigw-deployment"
   gateway_id     = local.apigw_ocid
-  path_prefix    = "/${var.prefix}"
+  path_prefix    = "/"
   specification {
     # Route the COMPUTE_PRIVATE_IP 
     routes {
