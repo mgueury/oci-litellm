2a82d0201c0c4c496000ebd6d3a1c51937c43aab
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-31 00:49:41 +0200

    v3.0

--- src/app/rest/start.sh
+++ src/app/rest/start.sh
@@ -5,5 +5,5 @@ cd $SCRIPT_DIR
 . ./env.sh
 
 source myenv/bin/activate
-# litellm --port 8080 --detailed_debug 2>&1 | tee litellm.log
+export PROXY_BASE_URL=https://${APIGW_HOSTNAME}
 litellm --port 8080 --config config.yaml --detailed_debug 2>&1 | tee rest.log
