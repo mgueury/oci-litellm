a539e57b4059cfbf63d11ecd343e25df1e88dab3
Marc Gueury <marcgueury@Marcs-MacBook-Pro.local>
2026-07-30 19:12:16 +0200

    v3.0

--- src/app/rest/start.sh
+++ src/app/rest/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
-. $HOME/compute/tf_env.sh
+. ./env.sh
 
 source myenv/bin/activate
-python rest.py 2>&1 | tee rest.log
+# litellm --port 8080 --detailed_debug 2>&1 | tee litellm.log
+litellm --port 8080 --config config.yaml --detailed_debug 2>&1 | tee rest.log
